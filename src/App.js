3c1452fb6a313b9a13d4dd9f7ac05f0e7c5ecfc2
Raj Poudel <raj.poudeel@gmail.com>
2023-06-08 23:09:18 +0545

    function is writeen for filtering the data from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import Jsondata from './MOCK_DATA.json'
 import './index.css'
 function App() {
+  const[searchTerm,setSearchTerm] = useState('')
+  console.log(searchTerm)
+
   return (
     <div className="App">
-      <input type="text" placeholder='search...' />
-      {Jsondata.map( (val,index) =>{
+      <input type="text" placeholder='search...' onChange={event => {setSearchTerm(event.target.value)}}/>
+      {Jsondata.filter((val) => {
+        if(searchTerm==""){
+        return val
+      }else if(val.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+        return val
+      }
+      }).map( (val,index) =>{
         return(
-
-          <div className='name'>
+ 
+          <div className='name' key={index}>
              {val.first_name}
 
           </div>
