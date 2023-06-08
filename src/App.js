39c8742ad196f36e5b6fc14344700bf978e3aedf
Raj Poudel <raj.poudeel@gmail.com>
2023-06-08 21:47:35 +0545

    importing api into js and into webpage using map method with two parameter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
-
+import Jsondata from './MOCK_DATA.json'
+import './index.css'
 function App() {
   return (
     <div className="App">
-     
+      <input type="text" placeholder='search...' />
+      {Jsondata.map( (val,index) =>{
+        return(
+
+          <div className='name'>
+             {val.first_name}
+
+          </div>
+        ) 
+      })}
     </div>
   );
 }
